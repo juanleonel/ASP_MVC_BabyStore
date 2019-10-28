ca4b115f987f012d8ffa3755b327cb5192ee3285
Juan L. Albores <juanl.albores.c@gmail.com>
2019-10-28 17:01:46 -0500

    modificacion para cargar multiples imagenes

--- BabyStore/Controllers/ProductImagesController.cs
+++ BabyStore/Controllers/ProductImagesController.cs
@@ -49,8 +49,126 @@ namespace BabyStore.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Upload(HttpPostedFileBase file)
+        public ActionResult Upload(HttpPostedFileBase[] files)
         {
+            bool allValid = true;
+            //string inValidFiles = "";
+            bool duplicates = false;
+            bool otherDbError = false;
+
+            System.Text.StringBuilder inValidFiles = new System.Text.StringBuilder();
+            //check the user has entered a file
+            if (files[0] != null)
+            {
+                //if the user has entered less than ten files
+                if (files.Length <= 10)
+                {
+                    foreach (var file in files)
+                    {
+                        if (!ValidateFile(file))
+                        {
+                            allValid = false;
+                            inValidFiles.Append(", " + file.FileName);
+                        }
+                    }
+                    //if they are all valid then try to save them to disk
+                    if (allValid)
+                    {
+                        foreach (var file in files)
+                        {
+                            try
+                            {
+                                SaveFileToDisk(file);
+                            }
+                            catch (Exception)
+                            {
+                                ModelState.AddModelError("FileName", "Sorry an error occurred saving the files to disk, please try again");
+                            }
+                        }
+                    }
+                    //else add an error listing out the invalid files
+                    else
+                    {
+                        ModelState.AddModelError("FileName", "All files must be gif, png, jpeg or jpg and less than 2MB in size.The following files" + inValidFiles + " are not valid");
+                    }
+                }
+                //the user has entered more than 10 files
+                else
+                {
+                    ModelState.AddModelError("FileName", "Please only upload up to ten files at a time");
+                }
+            }
+            else
+            {
+                //if the user has not entered a file return an error message
+                ModelState.AddModelError("FileName", "Please choose a file");
+            }
+            if (ModelState.IsValid)
+            {
+
+
+                System.Text.StringBuilder duplicateFiles = new System.Text.StringBuilder();
+                foreach (var file in files)
+                {
+                    //try and save each file
+                    var productToAdd = new Domain.ProductsImage { FileName = file.FileName };
+                    try
+                    {
+                        ProductImageDA.Create(productToAdd);                      
+                    }
+                    //if there is an exception check if it is caused by a duplicate file
+                    catch (Exception ex)
+                    {
+                        Exception BaseEx = ex.GetFirstException();
+
+                        if (BaseEx.GetBaseException().GetType() == typeof(System.Data.SqlClient.SqlException))
+                        {
+                            Int32 ErrorCode = ((System.Data.SqlClient.SqlException)BaseEx).Number;
+                            switch (ErrorCode)
+                            {
+                                case 2627:  // Unique constraint error     
+                                    duplicateFiles.Append(", " + file.FileName);
+                                    duplicates = true;                                    
+                                    break;
+                                case 547:   // Constraint check violation
+                                    break;
+                                case 2601:  // Duplicated key row error
+
+                                    break;
+                                default:
+                                    otherDbError = true;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            otherDbError = true;                            
+                        }
+                    }
+
+
+
+
+                }
+
+                //add a list of duplicate files to the error message
+                if (duplicates)
+                {
+                    ModelState.AddModelError("FileName", "All files uploaded except the files" + duplicateFiles.ToString() + ", which already exist in the system." + " Please delete them  and try again if you wish to re - add them");
+                    return View();
+                }
+                else if (otherDbError)
+                {
+                    ModelState.AddModelError("FileName", "Sorry an error has occurred saving to the  database, please try again");
+                    return View();
+                }
+
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+            /*
             if (file != null)
             {
                 if (ValidateFile(file))
@@ -95,12 +213,12 @@ namespace BabyStore.Controllers
                             case 547:   // Constraint check violation
                                 break;
                             case 2601:  // Duplicated key row error
-                                
+
                                 break;
                             default:
                                 break;
                         }
-                        
+
                     }
                     else
                     {
@@ -108,9 +226,9 @@ namespace BabyStore.Controllers
                     }
                     return View();
                 }        
-               
-            }
-            return View();
+
+            }*/
+           
         }
 
         // POST: ProductImages/Create
